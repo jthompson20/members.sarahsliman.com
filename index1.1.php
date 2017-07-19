5c3689ebaa4fc7a047a2884e33480713e7389efb
Matt Thompson <matt.thompson@stack.com>
2017-07-19 11:36:06 -0400

    v1.1

--- index1.1.php
+++ index1.1.php
@@ -840,11 +840,11 @@ h1.page-header {
 			<!-- Main Menu -->
 			<div class="side-menu-container">
 				<ul class="nav navbar-nav">
-					<li class="active"><a href="<?php echo $_SERVER['PHP_SELF']; ?>"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
-					<li><a href="https://www.facebook.com/groups/1442018715889142/" target="_blank"><span class="glyphicon glyphicon-plane"></span> Facebook Group</a></li>
-					<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?pg=recipes"><span class="glyphicon glyphicon-plane"></span> Personal Recipes</a></li>
-					<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?pg=workouts"><span class="glyphicon glyphicon-user"></span> Power Group Workouts</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-cloud"></span> Sarah's Vlog <small>(NEW)</small></a></li>
+					<li class="active"><a href="<?php echo $_SERVER['PHP_SELF']; ?>"><span class="glyphicon glyphicon-home"></span> Dashboard</a></li>
+					<li><a href="https://www.facebook.com/groups/1442018715889142/" target="_blank"><span class="glyphicon glyphicon-users"></span> Facebook Group</a></li>
+					<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?pg=recipes"><span class="glyphicon glyphicon-apple"></span> Personal Recipes</a></li>
+					<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?pg=workouts"><span class="glyphicon glyphicon-facetime-video"></span> Power Group Workouts</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-play"></span> Sarah's Vlog <small>(NEW)</small></a></li>
 
 
 				</ul>
